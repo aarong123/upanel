e9a28505fb4da355b52b09c87bfe2e40caa5841c
Andres Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-05-23 20:04:08 -0500

    Update web.php
    Actualizando rutas

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,25 @@ Route::group(['middleware' => ['auth']], function () {
         // rutas de roles
         Route::get('/rol', 'RolController@index');
 
+        // rutas de usuarios
+        Route::get('/user', 'UserController@index');
+        Route::get('/user/create', 'UserController@create');
+        Route::post('/user/register', 'UserController@store');
+        Route::get('/user/edit/{person}', 'UserController@edit');
+        Route::put('/user/update/{person}', 'UserController@update');
+        Route::delete('/user/trashed/active/{person}', 'UserController@active');
+        Route::delete('/user/deactive/{person}', 'UserController@deactive');
+        Route::get('/user/trashed', 'UserController@trashed');
 
-
-        //rutas de productos
-        Route::get('/items', 'ItemController@index');
-
-
+        // rutas de usuarios
+        Route::get('/category', 'CategoryController@index');
+        Route::get('/category/create', 'CategoryController@create');
+        Route::post('/category/register', 'CategoryController@store');
+        Route::get('/category/edit/{category}', 'CategoryController@edit');
+        Route::put('/category/update/{category}', 'CategoryController@update');
+        Route::delete('category/trashed/active/{category}', 'CategoryController@active');
+        Route::delete('/category/deactive/{category}', 'CategoryController@deactive');
+        Route::get('/category/trashed', 'CategoryController@trashed');
     });
 });
 
