7cf8297d7656328a4e556c1db529e24bb725e6c5
Andres Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-05-25 22:26:56 -0500

    Mejoras a la vista index de roles
    Se añadió card para mostrar mejor la tabla y las migas de pan

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -2,25 +2,43 @@
 
 @section('content')
 
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="{{ url('/main') }}">Home</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+                Roles
+            </li>
+        </ol>
+    </nav>
+
     <div class="container">
-        <table class="table table-hover table-dark">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Descripción</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($roles as $rol)
-                <tr>
-                    <th scope="row">{{ $rol->id }}</th>
-                    <td>{{ $rol->name }}</td>
-                    <td>{{ $rol->description }}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+        <div class="card">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                <table class="table table-hover table-dark" id="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Descripción</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($roles as $rol)
+                        <tr>
+                            <th scope="row">{{ $rol->id }}</th>
+                            <td>{{ $rol->name }}</td>
+                            <td>{{ $rol->description }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
 @stop
